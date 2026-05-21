22386df504edfaad259bdba81a125baf11515235
harmi4421-cell <harmi4421@gmail.com>
2026-05-21 16:22:47 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,18 @@
 <?php
-$conn = mysqli_connect("camping-db-service", "root", "MySecretPass123", "camping_db");
+$db_url = "postgresql://root:x0Ok0JBKoZOBljlsad5T5v4HwZFoHfFu@dpg-d87e4857vvec738v60ng-a/camping_db_i9iu";
 
-if (!$conn) {
-    die("Connection Failed: " . mysqli_connect_error());
+try {
+    $url = parse_url($db_url);
+    $host = $url["host"];
+    $port = $url["port"] ?? 5432;
+    $user = $url["user"];
+    $pass = $url["pass"];
+    $dbname = ltrim($url["path"], '/');
+
+    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection Failed: " . $e->getMessage());
 }
 
 session_start();
