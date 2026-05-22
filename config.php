5d7527dfdd75c3c739fbad96261283aff678adcd
harmi4421-cell <harmi4421@gmail.com>
2026-05-22 20:04:04 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,10 @@
 <?php
-$conn = mysqli_connect("camping-db-service", "root", "MySecretPass123", "camping_db", 3306);
+$host = "b2ugtp3gikow9pc5abij-mysql.services.clever-cloud.com";
+$user = "ue9i3yhwk0geiyun";
+$password = "IQK8oUFQ8x6oiPlbbKIX"; 
+$database = "b2ugtp3gikow9pc5abij";
+
+$conn = mysqli_connect($host, $user, $password, $database, 3306);
 
 if (!$conn) {
     die("Connection Failed: " . mysqli_connect_error());
